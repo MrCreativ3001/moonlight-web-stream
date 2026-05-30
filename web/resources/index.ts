2b635ad38997807899440968b2db1772179444d9
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2026-05-30 16:19:00 +0200

    fix images not showing up

--- web/resources/index.ts
+++ web/resources/index.ts
@@ -1,13 +1,22 @@
-import { buildUrl } from "../config_.js";
+import HOST_IMAGE from "./desktop_windows-48px.svg";
+import HOST_OVERLAY_OFFLINE from "./baseline-error_outline-24px.svg";
+import HOST_OVERLAY_LOCK from "./baseline-lock-24px.svg";
 
-export const HOST_IMAGE = buildUrl("/resources/desktop_windows-48px.svg")
+import WARN_IMAGE from "./baseline-warning-24px.svg";
+import ERROR_IMAGE from "./baseline-error_outline-24px.svg";
+
+import APP_NO_IMAGE from "./no_app_image.png";
+
+import LOGOUT_IMAGE from "./new/logout-svgrepo-com.svg";
+
+export {
+    HOST_IMAGE,
+    HOST_OVERLAY_OFFLINE,
+    HOST_OVERLAY_LOCK,
+    WARN_IMAGE,
+    ERROR_IMAGE,
+    APP_NO_IMAGE,
+    LOGOUT_IMAGE,
+};
+
 export const HOST_OVERLAY_NONE = "";
-export const HOST_OVERLAY_OFFLINE = buildUrl("/resources/baseline-error_outline-24px.svg")
-export const HOST_OVERLAY_LOCK = buildUrl("/resources/baseline-lock-24px.svg")
-
-export const WARN_IMAGE = buildUrl("/resources/baseline-warning-24px.svg")
-export const ERROR_IMAGE = buildUrl("/resources/baseline-error_outline-24px.svg")
-
-export const APP_NO_IMAGE = buildUrl("/resources/no_app_image.png")
-
-export const LOGOUT_IMAGE = buildUrl("/resources/new/logout-svgrepo-com.svg")

c52f2ff17a2ef5412501564e330112e51e17fbfc
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2026-07-30 23:13:24 +0200

    reimplemented webrtc audio

--- src/api/stream/webrtc/audio.rs
+++ src/api/stream/webrtc/audio.rs
@@ -1,11 +1,13 @@
 use std::sync::Arc;
 
 use bytes::Bytes;
-use moonlight_common::stream::tokio::MoonlightStream;
-use tokio::spawn;
+use moonlight_common::stream::{audio::AudioFrame, tokio::MoonlightStream};
+use tokio::{
+    spawn,
+    sync::mpsc::{UnboundedSender, unbounded_channel},
+};
 use tracing::{Instrument, debug_span, info, trace, warn};
 use webrtc::{
-    Error,
     peer_connection::RTCPeerConnection,
     rtp::{
         extension::{HeaderExtension, playout_delay_extension::PlayoutDelayExtension},
@@ -15,87 +17,99 @@ use webrtc::{
     track::track_local::track_local_static_rtp::TrackLocalStaticRTP,
 };
 
-use crate::api::stream::webrtc::opus_codec;
+use crate::{api::stream::webrtc::opus_codec, app::AppError};
 
-pub async fn add_audio_track(
-    peer: &RTCPeerConnection,
-    stream: &MoonlightStream,
-) -> Result<(), Error> {
-    // TODO: what audio format is used?
+// TODO: add audio over unreliable data channels using AudioStream and AudioPayloader
 
-    let config = stream.audio_setup();
+pub struct AudioChannel {
+    frame_sender: UnboundedSender<AudioFrame<Bytes>>,
+}
 
-    // Create audio track
-    let mut codec = opus_codec();
-    codec.channels = config.channel_count as u16;
+impl AudioChannel {
+    pub async fn new_track(
+        stream: &MoonlightStream,
+        peer: &RTCPeerConnection,
+    ) -> Result<Self, AppError> {
+        // TODO: what audio format is used?
 
-    let track = Arc::new(TrackLocalStaticRTP::new(
-        codec.clone(),
-        "audio".to_string(),
-        "moonlight".to_string(),
-    ));
+        let config = stream.audio_setup();
 
-    let audio_sender = peer.add_track(track.clone()).await?;
+        // Create audio track
+        let mut codec = opus_codec();
+        codec.channels = config.channel_count as u16;
 
-    // Feedback
-    spawn(
-        async move {
-            let mut buffer = [0; 1500];
+        let track = Arc::new(TrackLocalStaticRTP::new(
+            codec.clone(),
+            "audio".to_string(),
+            "moonlight".to_string(),
+        ));
 
-            while let Ok((_packets, _)) = audio_sender.read(&mut buffer).await {
-                // do nothing, because we'll just have to poll packets to dequeue them
-            }
-        }
-        .instrument(debug_span!("audio rtcp feedback")),
-    );
+        let audio_sender = peer.add_track(track.clone()).await?;
 
-    info!(audio_config = ?config, codec = ?codec, "finished audio track setup");
+        // Feedback
+        spawn(
+            async move {
+                let mut buffer = [0; 1500];
 
-    spawn({
-        let stream = stream.clone();
-
-        async move {
-            let mut sequence_number = 0u16;
-
-            while let Ok(frame) = stream.poll_audio_frame().await {
-                let timestamp = (frame.timestamp.as_millis() * 48) as u32;
-
-                if track.all_binding_paused().await {
-                    trace!("audio track all binding paused");
-                    // Don't send any packets when the track is paused because we don't want to increment the sequence number
-                    return;
+                while let Ok((_packets, _)) = audio_sender.read(&mut buffer).await {
+                    // do nothing, because we'll just have to poll packets to dequeue them
                 }
+            }
+            .instrument(debug_span!("audio rtcp feedback")),
+        );
 
-                trace!(len = ?frame.buffer.len(), timestamp = ?frame.timestamp, "audio frame");
+        info!(audio_config = ?config, codec = ?codec, "finished audio track setup");
 
-                sequence_number = sequence_number.wrapping_add(1);
+        let (frame_sender, mut frame_receiver) = unbounded_channel::<AudioFrame<Bytes>>();
 
-                // Opus doesn't need any special payloading: https://github.com/webrtc-rs/webrtc/blob/6b94718e23111df28125f96af4b0de8cbb3dfd0d/rtp/src/codecs/opus/mod.rs#L9-L24
-                if let Err(err) = track
-                    .write_rtp_with_extensions(
-                        &Packet {
-                            header: Header {
-                                version: 2,
-                                sequence_number,
-                                timestamp,
-                                payload_type: 111,
-                                ..Default::default()
+        spawn(
+            async move {
+                let mut sequence_number = 0u16;
+
+                while let Some(frame) = frame_receiver.recv().await {
+                    let timestamp = (frame.timestamp.as_millis() * 48) as u32;
+
+                    if track.all_binding_paused().await {
+                        trace!("audio track all binding paused");
+                        // Don't send any packets when the track is paused because we don't want to increment the sequence number
+                        return;
+                    }
+
+                    trace!(len = ?frame.buffer.len(), timestamp = ?frame.timestamp, "audio frame");
+
+                    sequence_number = sequence_number.wrapping_add(1);
+
+                    // Opus doesn't need any special payloading: https://github.com/webrtc-rs/webrtc/blob/6b94718e23111df28125f96af4b0de8cbb3dfd0d/rtp/src/codecs/opus/mod.rs#L9-L24
+                    if let Err(err) = track
+                        .write_rtp_with_extensions(
+                            &Packet {
+                                header: Header {
+                                    version: 2,
+                                    sequence_number,
+                                    timestamp,
+                                    payload_type: 111,
+                                    ..Default::default()
+                                },
+                                payload: Bytes::from_owner(frame.buffer),
                             },
-                            payload: Bytes::from_owner(frame.buffer),
-                        },
-                        &[HeaderExtension::PlayoutDelay(PlayoutDelayExtension {
-                            min_delay: 0,
-                            max_delay: 0,
-                        })],
-                    )
-                    .await
-                {
-                    warn!(error = %err, "failed to send audio frame");
+                            &[HeaderExtension::PlayoutDelay(PlayoutDelayExtension {
+                                min_delay: 0,
+                                max_delay: 0,
+                            })],
+                        )
+                        .await
+                    {
+                        warn!(error = %err, "failed to send audio frame");
+                    }
                 }
             }
-        }
-        .instrument(debug_span!("relay: audio"))
-    });
+            .instrument(debug_span!("audio frame relay")),
+        );
 
-    Ok(())
+        Ok(Self { frame_sender })
+    }
+
+    pub fn on_frame(&mut self, frame: AudioFrame<Bytes>) {
+        let _ = self.frame_sender.send(frame);
+    }
 }
